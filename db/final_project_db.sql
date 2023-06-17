5ee1fad95c58ff9c30a29e2e7c7395ac81b4ed84
Konstantinos Tsakaleris <13333629+tsakalerisk@users.noreply.github.com>
2023-06-18 00:17:49 +0300

    Implement database

--- db/final_project_db.sql
+++ db/final_project_db.sql
@@ -1,29 +1,33 @@
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`final_project` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE final_project;
-
 DROP TABLE IF EXISTS `people`;
+
 CREATE TABLE people (
   `PersonID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `FirstName` VARCHAR(255) NOT NULL,
-  `LastName` VARCHAR(255) NOT NULL,
-  `Email` VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `FirstName` VARCHAR(255) NOT NULL DEFAULT '',
+  `LastName` VARCHAR(255) NOT NULL DEFAULT '',
+  `Email` VARCHAR(255) NOT NULL DEFAULT ''
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 DROP TABLE IF EXISTS `items`;
+
+-- ImageUrl is TEXT instead of VARCHAR so that we can store big urls
 CREATE TABLE items (
   `ItemID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  Name VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Name` VARCHAR(255) NOT NULL,
+  `Price` DECIMAL(10, 2) NOT NULL,
+  `ImageUrl` TEXT
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 DROP TABLE IF EXISTS `orders`;
+
 CREATE TABLE orders (
   `OrderID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `PersonID` INT UNSIGNED NOT NULL,
   `OrderDate` DATETIME NOT NULL,
   FOREIGN KEY (PersonID) REFERENCES people (PersonID)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 DROP TABLE IF EXISTS `order_details`;
+
 CREATE TABLE order_details (
   `OrderDetailsID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `OrderID` INT UNSIGNED NOT NULL,
@@ -31,5 +35,37 @@ CREATE TABLE order_details (
   `Quantity` INT,
   FOREIGN KEY (OrderID) REFERENCES orders (OrderID),
   FOREIGN KEY (ItemID) REFERENCES items (ItemID)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
+-- load mock data
+LOAD DATA INFILE '/var/lib/mysql-files/people.csv'
+INTO TABLE people
+FIELDS TERMINATED BY ','
+ENCLOSED BY "'"
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(FirstName, LastName, Email);
+
+LOAD DATA INFILE '/var/lib/mysql-files/items.csv'
+INTO TABLE items
+FIELDS TERMINATED BY ','
+ENCLOSED BY "'"
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(Name, Price, ImageUrl);
+
+LOAD DATA INFILE '/var/lib/mysql-files/orders.csv'
+INTO TABLE orders
+FIELDS TERMINATED BY ','
+ENCLOSED BY "'"
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(PersonID, OrderDate);
+
+LOAD DATA INFILE '/var/lib/mysql-files/order_details.csv'
+INTO TABLE order_details
+FIELDS TERMINATED BY ','
+ENCLOSED BY "'"
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(OrderID, ItemID, Quantity);
